2c5391c5230f3f4ff8b15ffe03c955b1b6092ebc
tusharp2e <tushar.bansal@mai.io>
2025-03-18 16:13:48 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { Network, submitTransaction, evaluateTransaction } from "kalp-wallet-ts";
+import { PolygonConnect } from "./PolygonConnect";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="container">
+      <hr className="divider" />
+      <div className="card">
+        <h2>Polygon Wallet</h2>
+        <PolygonConnect />
+      </div>
+    </div>
+  );
+}
+
+export default App;
